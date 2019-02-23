43d882bf6a362e9438645f25fb92051ce17a6078
wades1248 <wades1248@gmail.com>
2019-02-22 23:24:55 -0600

    should be final

--- index.js
+++ index.js
@@ -8,23 +8,33 @@ delete item when "delete" is clicked,
 ensure good semantic HTML is being used and that buttons are accessable,*/
 
 function runList(){
-	$("#js-shopping-list-form").submit(event => {
-		event.preventDefault();
-		const usrInput = $("#shopping-list-entry").val();
-		$(".shopping-list").append( 	
-				`<li>
-        <span class="shopping-item shopping-item">${usrInput}</span>
-        <div class="shopping-item-controls">
-          <button class="shopping-item-toggle">
-            <span class="button-label">check</span>
-          </button>
-          <button class="shopping-item-delete">
-            <span class="button-label">delete</span>
-          </button>
-        </div>
-      </li>`);
-      }/*event function close*/
-      )/*submit close*/
-      }/*runList close*/
- $(runList())
-	
+
+ $("#js-shopping-list-form").submit(event => {
+   event.preventDefault();
+   const usrInput = $("#shopping-list-entry").val();
+    $(".shopping-list").append(   
+        `<li>
+							<span class="shopping-item shopping-item">${usrInput}</span>
+								<div class="shopping-item-controls">
+									<button class="shopping-item-toggle">
+										<span class="button-label">check</span>
+									</button>
+									<button class="shopping-item-delete">
+										<span class="button-label">delete</span>
+									</button>
+								</div>
+							</li>`);
+						}/*event function close*/
+					)/*submit close*/
+
+ $(".shopping-item-toggle").click(event => {
+	$(event.target).closest("li").toggleClass("shopping-item__checked");
+		}/*event function close*/
+	)/*submit close*/
+	$(".shopping-item-delete").click(event => {
+		$(event.target).closest("li").detach();})
+
+}/*runList close*/
+
+$(runList())
+
