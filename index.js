b17ddf06d9681c71651df04ed13ede3bf49ee9b1
wades1248 <wades1248@gmail.com>
2019-02-22 20:44:10 -0600

    added input function in JS

---
+++ index.js
@@ -0,0 +1,30 @@
+/*Things to do in JS:
+Prevent default form submission,
+make form submission result in a new <li> element (copy and paste some code)
+ensure that forms enter on "return" and "click",
+add class "shopping-item_checked" upon clicking check buttom. (.toggleClass)
+remove "shopping-item_checkied" if buttom is checked again
+delete item when "delete" is clicked,
+ensure good semantic HTML is being used and that buttons are accessable,*/
+
+function runList(){
+	$("#js-shopping-list-form").submit(event => {
+		event.preventDefault();
+		const usrInput = $("#shopping-list-entry").val();
+		$(".shopping-list").append( 	
+				`<li>
+        <span class="shopping-item shopping-item">${usrInput}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`);
+      }/*event function close*/
+      )/*submit close*/
+      }/*runList close*/
+ $(runList())
+	
